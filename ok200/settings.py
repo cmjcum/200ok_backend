224d96d137cd7c9768c76773152e49b215ed9359
yinmsk <galaxy220111@gmail.com>
2022-07-06 06:07:05 +0900

    CLN

--- ok200/settings.py
+++ ok200/settings.py
@@ -88,11 +88,15 @@ WSGI_APPLICATION = 'ok200.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ok200',
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
